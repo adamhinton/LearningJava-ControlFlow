2f6b6116c9219562c39f4a35d77fdef9ec33f8bd
adamd <adam.d.hinton@gmail.com>
2024-02-15 00:25:42 -0500

    working on divisor challenge

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        int validNumCount = 0;
+
+        for(int i=1; i<=1000; i++){
+        }
+
+    }
 
 
 }
