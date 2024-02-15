52c4d3609967a6ce78c1ac4cfb0cad1e4eafd773
adamd <adam.d.hinton@gmail.com>
2024-02-15 11:58:04 -0500

    first try at challenge solxn

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,23 @@
 public class Main {
     public static void main(String[] args) {
 
+        int myNum = 5;
+
+        while(myNum <=20){
+            if(!isEvenNumber(myNum)){
+                myNum++;
+                continue;
+            }
+            System.out.println(myNum);
+            myNum++;
+        }
+
     }
+
+    public static boolean isEvenNumber (int num){
+        return num % 2 == 0;
+    }
+
 
 
 }
